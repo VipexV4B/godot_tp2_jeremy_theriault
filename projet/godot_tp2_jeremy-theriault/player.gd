4d58516770268d16cdf8f8fc4ec7d310602c8fbf
VipexV4B <jeremytheriault23@gmail.com>
2025-09-26 21:04:11 -0400

    player

--- projet/godot_tp2_jeremy-theriault/player.gd
+++ projet/godot_tp2_jeremy-theriault/player.gd
@@ -1,20 +1,50 @@
-extends Node2D
+extends CharacterBody2D
 
-@onready var anim = $CharacterBody2D/Sprite_player
+@onready var anim = $Sprite_player
+@onready var animation = $sprite_idle
 
-const JUMP_FORCE = -450
-const velocity = 
+const SPEED = 200.0
+const SPEED_RUN = 300.0
+const JUMP_FORCE = -400.0
+const GRAVITY = 900.0
 
-func _process(_delta)
+func _physics_process(delta: float) -> void:
+	# gravité
+	if not is_on_floor():
+		velocity.y += GRAVITY * delta
+
+	var direction := 0.0
+
+	# Déplacements horizontaux
 	if Input.is_action_pressed("move_right"):
-		anim.play("walk")
-	elif Input.is_action_pressed("move_left"):
-		anim.play("walk")
-	elif Input.is_action_pressed("jump"):
-		anim.play("jump")
-	elif Input.is_action_pressed("shield"):
-		anim.play("shield")
-	elif Input.is_action_pressed("shift"):
-		anim.play("run")
+		direction += 1
+		anim.flip_h = false
+	if Input.is_action_pressed("move_left"):
+		direction -= 1
+		anim.flip_h = true
+
+	velocity.x = direction * SPEED
+
+	# Saut
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_FORCE
+
+	# Déplacement
+	move_and_slide()
+
+	# Animations
+	if not is_on_floor():
+		anim.show()
+		animation.hide()
+		if anim.animation != "jump":
+			anim.play("jump")
+
+	elif direction == 0 and is_on_floor():
+		anim.hide()
+		animation.show()
+		animation.play("idle")
+
 	else:
-		anim.play("idle")
+		anim.show()
+		animation.hide()
+		anim.play("walk")
